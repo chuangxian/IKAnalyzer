6ab2884ce602d04c06b989a25141ebb91574c688
袁凡迪 <fandi.yuan@mingyizhudao.com>
2017-05-22 17:32:47 +0800

    修改demo代码以适应6.5

--- src/main/java/org/wltea/analyzer/sample/LuceneIndexAndSearchDemo.java
+++ src/main/java/org/wltea/analyzer/sample/LuceneIndexAndSearchDemo.java
@@ -82,7 +82,7 @@ public class LuceneIndexAndSearchDemo {
       directory = new RAMDirectory();
 
       // 配置IndexWriterConfig
-      IndexWriterConfig iwConfig = new IndexWriterConfig(Version.LUCENE_43, analyzer);
+      IndexWriterConfig iwConfig = new IndexWriterConfig(analyzer);
       iwConfig.setOpenMode(OpenMode.CREATE_OR_APPEND);
       iwriter = new IndexWriter(directory, iwConfig);
       // 写入索引
@@ -99,7 +99,7 @@ public class LuceneIndexAndSearchDemo {
 
       String keyword = "中文分词工具包";
       // 使用QueryParser查询分析器构造Query对象
-      QueryParser qp = new QueryParser(Version.LUCENE_43, fieldName, analyzer);
+      QueryParser qp = new QueryParser(fieldName, analyzer);
       qp.setDefaultOperator(QueryParser.AND_OPERATOR);
       Query query = qp.parse(keyword);
       System.out.println("Query = " + query);
